04451b3b4b6f328c7c303ddc892688a7fc4ad416
Nicholas <poontzejian@gmail.com>
2025-03-07 08:46:32 +0800

    .

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { motion } from "framer-motion";
 import { ResumeData } from "../Header";
 import "./Resume.css";
@@ -8,120 +7,103 @@ type ResumeProps = {
   data: ResumeData["resume"];
 };
 
-class Resume extends Component<ResumeProps> {
-  render() {
-    if (!this.props.data) return null;
+const Resume: React.FC<ResumeProps> = ({ data }) => {
+  if (!data) return null;
 
-    const educationElements = this.props.data.education.map(function (
-      education
-    ) {
-      return (
-        <div key={education.school}>
-          <h3 className="org-name">{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p className="desc">{education.description}</p>
+  const educationElements = data.education.map((education) => (
+    <div key={education.school} className="resume-item secondary-text">
+      <h3 className="resume-item-title">{education.school}</h3>
+      {/* <p className="resume-item-subtitle">
+        {education.degree} <span className="bullet">&bull;</span>{" "}
+        <em className="resume-date">{education.graduated}</em>
+      </p>
+      <p className="resume-item-description">{education.description}</p> */}
+    </div>
+  ));
+
+  const workElements = data.work.map((work) => (
+    <div key={work.company} className="resume-item secondary-text">
+      <h3 className="resume-item-title">{work.company}</h3>
+      {/* <p className="resume-item-subtitle">
+        {work.title} <span className="bullet">&bull;</span>{" "}
+        <em className="resume-date">{work.years}</em>
+      </p>
+      <p className="resume-item-description">{work.description}</p> */}
+    </div>
+  ));
+
+  const programmingLanguageElements = data.programminglanguages.map(
+    (language) => (
+      <li className="programming-language-item" key={language.name}>
+        <em>{language.name}</em>
+      </li>
+    )
+  );
+
+  const animationProps = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0 },
+    transition: { duration: 0.7 },
+  };
+
+  return (
+    <section id="resume">
+      <motion.div {...animationProps} className="resume-section-container">
+        <div className="resume-row">
+          <div className="resume-header-column">
+            <h1 className="resume-section-title">
+              <Text fontSize="2xl">Education</Text>
+              <div className="" />
+            </h1>
+          </div>
+
+          <div className="resume-content-column">
+            <div className="resume-content-wrapper">
+              <div className="resume-content">{educationElements}</div>
+            </div>
+          </div>
         </div>
-      );
-    });
+      </motion.div>
 
-    const workElements = this.props.data.work.map(function (work) {
-      return (
-        <div key={work.company}>
-          <h3 className="org-name">{work.company}</h3>
-          <p className="info">
-            {work.title}
-            <span>&bull;</span> <em className="date">{work.years}</em>
-          </p>
-          <p className="desc">{work.description}</p>
+      <hr className="section-divider" />
+
+      <motion.div {...animationProps} className="resume-section-container">
+        <div className="resume-row">
+          <div className="resume-header-column">
+            <h1 className="resume-section-title">
+              <Text fontSize="2xl">Work</Text>
+              <div className="" />
+            </h1>
+          </div>
+
+          <div className="resume-content-column">
+            <div className="resume-content-wrapper">
+              <div className="resume-content">{workElements}</div>
+            </div>
+          </div>
         </div>
-      );
-    });
+      </motion.div>
 
-    const programElement = this.props.data.programminglanguages.map(
-      (programminglanguages) => {
-        return (
-          <li className="programming-lang" key={programminglanguages.name}>
-            <em>{programminglanguages.name}</em>
-          </li>
-        );
-      }
-    );
+      <hr className="section-divider" />
 
-    const animationProps = {
-      initial: { opacity: 0, x: -100 },
-      animate: { opacity: 1, x: 0 },
-      transition: { duration: 0.7 },
-    };
-
-    return (
-      <section id="resume">
-        <motion.div
-          {...animationProps}
-          className="education-container constrain-box"
-        >
-          <div className="row education">
-            <div className="three columns header-col">
-              <h1 className="primary-text">
-                <Text fontSize="2xl">Education</Text>
-                <div className="pulsing-bar" />
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="main-col secondary-text">
-                  {educationElements}
-                </div>
-              </div>
-            </div>
+      <motion.div {...animationProps} className="resume-section-container">
+        <div className="resume-row">
+          <div className="resume-header-column">
+            <h1 className="resume-section-title">
+              <Text fontSize="2xl">Programming Languages</Text>
+              <div className="" />
+            </h1>
           </div>
-        </motion.div>
 
-        <hr className="section-divider" />
-
-        <motion.div {...animationProps} className="row skill constrain-box">
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1 className="primary-text">
-                <Text fontSize="2xl">Work</Text>
-                <div className="pulsing-bar" />
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns main-col secondary-text">
-                  {workElements}
-                </div>
-              </div>
-            </div>
+          <div className="resume-content-column">
+            <ul className="programming-language-list">
+              {programmingLanguageElements}
+            </ul>
           </div>
-        </motion.div>
-
-        <hr className="section-divider" />
-
-        <motion.div {...animationProps} className="row skill constrain-box">
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1 className="primary-text">
-                <Text fontSize="2xl">Programming Languages</Text>
-                <div className="pulsing-bar" />
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <div>
-                <ul>{programElement}</ul>
-              </div>
-            </div>
-          </div>
-        </motion.div>
-      </section>
-    );
-  }
-}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Resume;
